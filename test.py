d1b9f674534b69ce8eb051ef2af85dc4a7712425
phischolz <33486489+phischolz@users.noreply.github.com>
2021-04-21 13:57:35 +0200

    Expanded test cases

--- test.py
+++ test.py
@@ -3,15 +3,29 @@ from horsepad import HorsePad
 import reachability
 
 
-class MyTestCase(unittest.TestCase):
-    def test_default_horsepad_zero(self):
-        pad = HorsePad()
-        self.assertEqual(pad.is_within((0, 0)), True)
+class DefaultHpTest(unittest.TestCase):
+    pad = HorsePad()
 
-    def test_default_horsepad_oob(self):
-        pad = HorsePad()
-        self.assertEqual(pad.is_within((-1, -1)), False)
-        self.assertEqual(pad.is_within((0, 3)), False)
+    def test_zero(self):
+        self.assertEqual(self.pad.is_within((0, 0)), True)
+
+    def test_oob(self):
+        self.assertEqual(self.pad.is_within((-1, -1)), False)
+        self.assertEqual(self.pad.is_within((0, 4)), False)
+
+    def test_blocked(self):
+        self.assertEqual(self.pad.is_within((0, 3)), False)
+
+    def test_moves(self):
+        self.assert_(self.pad.move((0, 0)))
+        self.assertEqual(self.pad.loc(), (0, 0))
+        self.assertNotEqual(self.pad.loc(), (1, 1))
+        self.assert_(self.pad.move((1, 1)))
+        self.assertEqual(self.pad.loc(), (1, 1))
+        self.assert_(not self.pad.move((5, 5)))
+        self.assertEqual(self.pad.loc(), (1, 1))
+
+# TODO non-default HPs
 
 
 if __name__ == '__main__':
